1fbca23c15b3eed4f05bf9bf2f336925042091d7
shudl <shudl@outlook.com>
2026-03-22 14:37:43 +0800

    Initial commit

---
+++ src/main/java/com/nexusmall/mall/NexusmallApplication.java
@@ -0,0 +1,13 @@
+package com.nexusmall.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusmallApplication.class, args);
+    }
+
+}
